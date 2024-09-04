cb435005050f00f34ed7f4e9098e39d198b8bb68
Elijah Hilty <elijah.hilty@hexagon.com>
2024-09-04 13:10:23 -0500

    Add message option

--- multicommit/__init__.py
+++ multicommit/__init__.py
@@ -2,14 +2,17 @@ from git import Repo, Head
 
 import re
 import os
+import click
 
 SUFFIX_PATTERN = re.compile(r"^(?P<root>(?:[a-zA-Z-_/])*)_(?:(?:[A-Z])+_)*(?:[A-Z])+")
 
-def main():
+@click.command()
+@click.option("--message", "-m", type=str, required=True)
+def main(message: str):
     # Assume current working dir is a repo
     repo = Repo(os.getcwd())
     # Commit!
-    repo.index.commit("Test message")
+    repo.index.commit(message)
     # If active branch is "subbranch", cherry-pick commit onto parent branch as well
     active_branch = repo.active_branch
     match = SUFFIX_PATTERN.match(active_branch.path)
