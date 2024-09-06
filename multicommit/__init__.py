40c12fbc3262c757924ed1ac8d0823056e6e3847
Elijah Hilty <elijah.hilty@hexagon.com>
2024-09-06 12:27:43 -0500

    Forward arguments directly to commit command

--- multicommit/__init__.py
+++ multicommit/__init__.py
@@ -1,21 +1,22 @@
 from git import Repo, Head
 
 import re
-import click
+import sys
 
 SUFFIX_PATTERN = re.compile(r"^(?P<root>(?:[a-zA-Z-_/])*)_(?:(?:[A-Z])+_)*(?:[A-Z])+")
 
-@click.command()
-@click.option("--message", "-m", type=str, required=True)
-def main(message: str):
-    repo = Repo(search_parent_directories=True)
-    # Commit!
-    repo.index.commit(message)
+def multicommit(repo: Repo, args: list[str]):
+    # Forward arguments to commit command
+    repo.git.commit(*args)
     # If active branch is "subbranch", cherry-pick commit onto parent branch as well
     active_branch = repo.active_branch
     match = SUFFIX_PATTERN.match(active_branch.path)
     if match:
         root_branch = Head(repo, match.groupdict()["root"])
         root_branch.checkout()
-        repo.git.__getattr__("cherry-pick")(active_branch.path)
+        repo.git._call_process("cherry-pick", active_branch.path)
         active_branch.checkout()
+
+def main():
+    repo = Repo(search_parent_directories=True)
+    multicommit(repo, sys.argv[1:])
